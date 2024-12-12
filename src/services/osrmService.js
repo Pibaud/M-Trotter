89adc7367378dc0976e2f213263973bb22dd09b2
HugoHal <HHalablian@gmail.com>
2024-12-12 17:03:42 +0100

    un peu de commentaires

--- src/services/osrmService.js
+++ src/services/osrmService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const config = require('../config/default');
 
-exports.getRoute = async (start, end, mode = 'driving') => {
+exports.getRoute = async (start, end, mode) => { //  différents mode : driving, foot, bike
     try {
         const url = `${config.osrmUrl}/route/v1/${mode}/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&geometries=geojson`;
         const response = await axios.get(url);
