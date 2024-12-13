d72471dc73de8ad0ae247cf5cff172458388974d
HugoHal <HHalablian@gmail.com>
2024-12-13 14:42:45 +0100

    en francais

--- src/services/mapHopperService.js
+++ src/services/mapHopperService.js
@@ -4,7 +4,7 @@ const API_URL = 'https://graphhopper.com/api/1/route';
 
 exports.getRoute = async (start, end, mode) => {
     try {
-        const URL = `${API_URL}?point=${start[0]},${start[1]}&point=${end[0]},${end[1]}&profile=${mode}&locale='fr'&instructions=true&calc_points=true&points_encoded=false&key=${process.env.MAPHOPPER_API_KEY}`;
+        const URL = `${API_URL}?point=${start[0]},${start[1]}&point=${end[0]},${end[1]}&profile=${mode}&locale=fr&instructions=true&calc_points=true&points_encoded=false&key=${process.env.MAPHOPPER_API_KEY}`;
         const response = await axios.get(URL);
 
         console.log('response', response.data);
