d08203bf054ac1d52fd4dc4e458eab8d11a1915c
hugohal <hhalablian@gmail.com>
2025-02-04 20:09:26 +0100

    la value de ce push est phénoménal

--- src/controllers/routeController.js
+++ src/controllers/routeController.js
@@ -32,7 +32,8 @@ exports.calculateRoute = async (req, res, next) => {
             }
 
             // Appel du service de transport en commun avec les ID trouvés
-            const transit = await getTransit(startLocation.id, endLocation.id, date, time, startLocation.pointType, endLocation.pointType);
+            let transit = await getTransit(startLocation.id, endLocation.id, date, time, startLocation.pointType, endLocation.pointType);
+            transit = rangeJson(transit);
             return res.status(200).json(transit);
         } else {
             // Calcul d'itinéraire pour les autres modes (ex: marche, vélo, voiture)
@@ -42,4 +43,56 @@ exports.calculateRoute = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+function rangeJson(json) {
+    if (json["Status"]["Code"] != "OK") {
+        return "Problème pour touver un itinéraire";
+    }
+    res = [];
+    trips = json["trips"]["Trip"];
+    for (let i =0; i<1; i++) {
+        elem = trips[i];
+        console.log("pour le ",i,"eme trajet : ",elem["sections"]["Section"]);
+        partis = [];
+        for (let j = 0; j<elem["sections"]["Section"].length; j++) {
+            let sexion  = elem["sections"]["Section"][j];
+            if (sexion["Leg"]["TransportMode"] == 'WALK'){
+                console.log("cette partie est à pied : ", sexion["Leg"])
+                chemins = [];
+                for (let k = 0; k<sexion["Leg"]["pathLinks"]["PathLink"].length; k++) {
+                    chemins.push(sexion["Leg"]["pathLinks"]["PathLink"][k]["Departure"]["Site"]["Position"]);
+                }
+                sexionparti = {
+                    chemin : chemins,
+                    arrivée : {nom : sexion["Leg"]["Arrival"]["Site"]["Name"], position : sexion["Leg"]["Arrival"]["Site"]["Position"]},
+                    temps : sexion["Leg"]["Duration"]
+                };
+            } else {
+                console.log("cette partie est en transit : ", sexion["PTRide"]["steps"]["Step"][0]["Arrival"]["StopPlace"])
+                arrets = []
+                for (let k = 0; k<sexion["PTRide"]["steps"]["Step"].length; k++){
+                    arrets.push([sexion["PTRide"]["steps"]["Step"][k]["Arrival"]["StopPlace"]["Name"], sexion["PTRide"]["steps"]["Step"][k]["Arrival"]["StopPlace"]["Position"]])
+                }
+                sexionparti = {
+                    ligne : sexion["PTRide"]["Line"]["Name"],
+                    Sens : sexion["PTRide"]["Destination"],
+                    étape : arrets,
+                    arrivée : sexion["PTRide"]["Arrival"],
+                    temps : sexion["PTRide"]["Duration"]
+                }
+            }
+            partis.push(sexionparti);
+        }
+        trip = {
+        "heure de départ : " :  elem["DepartureTime"],
+        "heure d'arrivée : " : elem["ArrivalTime"],
+        "distance : " : elem["Distance"],
+        "co2 economisé : " : elem["CarbonFootprint"]["CarCO2"] - elem["CarbonFootprint"]["TripCO2"],
+        "trajet : " : partis
+        };
+        res.push(trip);
+    }
+    console.log("res : ", res);
+    return res;
 };
