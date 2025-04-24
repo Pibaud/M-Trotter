e0e8963b6d4361842d6e905982e243711667cb1c
HugoHal <HHalablian@gmail.com>
2025-04-24 13:30:24 +0200

    Add updateAvis route to avis.js for updating reviews

--- src/routes/avis.js
+++ src/routes/avis.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { postAvis, getAvisByPlaceId, deleteAvis, likeAvis, unlikeAvis, getAvisbyUser } = require('../controllers/avisController');
+const { postAvis, getAvisByPlaceId, deleteAvis, likeAvis, unlikeAvis, getAvisbyUser, updateAvis } = require('../controllers/avisController');
 const upload = require('../middleware/multerMiddleware'); // Import de Multer
 
 router.post('/postavis', upload.single('photo'), postAvis);
 router.post('/getavis', getAvisByPlaceId); // On recherche maintenant par place_id
 router.post('/mesavis', getAvisbyUser); // On recherche les avis d'un utilisateur
 router.post('/deleteavis', deleteAvis); // Route pour supprimer un avi
+router.post('/updateavis', updateAvis); // Route pour mettre à jour un avis
 
 router.post('/likeavis', likeAvis); // Route pour liker un avis
 router.post('/unlikeavis', unlikeAvis); // Route pour unliker un avis
