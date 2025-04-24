0256c442e7e8c31a71c86e11c3d3fcf1eaf75014
HugoHal <HHalablian@gmail.com>
2025-04-24 11:09:03 +0200

    Refactor user voting logic in getImagesByPlaceId to simplify query and improve performance

--- src/models/uploadModel.js
+++ src/models/uploadModel.js
@@ -9,16 +9,13 @@ const getImagesByPlaceId = async (id_lieu, user_id) => {
                 p.id_lieu, 
                 p.id_avis,
                 COALESCE(SUM(g.vote_type * LOG(COALESCE(u.fiabilite, 0) + 1)), 0) AS weighted_vote_score,
-                CASE 
-                    WHEN gv.id_user IS NOT NULL THEN true 
-                    ELSE false 
-                END AS user_has_voted
+                COALESCE(gv.vote_type, 0) AS user_has_voted
              FROM photos p
              LEFT JOIN goodimage g ON p.id_photo = g.id_image
              LEFT JOIN users u ON g.id_user = u.id
              LEFT JOIN goodimage gv ON p.id_photo = gv.id_image AND gv.id_user = $2
              WHERE p.id_lieu = $1
-             GROUP BY p.id_photo, p.id_lieu, p.id_avis, gv.id_user
+             GROUP BY p.id_photo, p.id_lieu, p.id_avis, gv.vote_type
              ORDER BY weighted_vote_score DESC, p.id_photo ASC`,
             [id_lieu, user_id]
         );
